a77d5cbc0d10941896070e89fb51a4f6f0e2941e
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-03-25 16:31:00 +0530

    Division : division functionality

--- src/Pratice.java
+++ src/Pratice.java
@@ -1,13 +1,15 @@
 public class Pratice {
     public static void main(String[] args) {
         System.out.println("Welcome to practice project");
-        int a=10,b=20,c,c1,c2;
+        int a=10,b=20,c,c1,c2,c3;
         c = a + b;
         System.out.println(c);
         c1 = a - b;
         System.out.println(c1);
         c2 = a * b;
         System.out.println(c2);
+        c3 = b/a;
+        System.out.println(c3);
     }
 
 }
