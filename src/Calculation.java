e2b23a532c06a1c7629d87262a62a058c308a496
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-03-25 15:46:18 +0530

    Added: Addtiion functionality added

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Calculation {
+public class Calculation {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Pratice.java
+++ src/Pratice.java
@@ -1,5 +1,9 @@
 public class Pratice {
     public static void main(String[] args) {
         System.out.println("Welcome to practice project");
+        int a=10,b=20,c;
+        c = a + b;
+        System.out.println(c);
     }
+
 }
