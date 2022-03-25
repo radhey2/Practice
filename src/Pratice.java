ae37a9da6e361d10893299eb46e2657ff950fc6c
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-03-25 15:54:53 +0530

    Subtration : Subtration Functionality Addded

--- src/Pratice.java
+++ src/Pratice.java
@@ -1,9 +1,11 @@
 public class Pratice {
     public static void main(String[] args) {
         System.out.println("Welcome to practice project");
-        int a=10,b=20,c;
+        int a=10,b=20,c,c1;
         c = a + b;
         System.out.println(c);
+        c1 = a - b;
+        System.out.println(c1);
     }
 
 }
